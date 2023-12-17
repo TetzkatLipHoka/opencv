354ca1c9f4d4e8610d0144cf6de26142e8e0fadc
TetzkatLipHoka <TLH@freenet.de>
2023-12-18 00:14:45 +0100

    Add files via upload

---
+++ 3rdparty/TesserAct/config_auto.h
@@ -0,0 +1,14 @@
+/* Define to 1 if you have the `feenableexcept' function. */
+/* #undef HAVE_FEENABLEEXCEPT */
+
+
+/* Version number */
+#define PACKAGE_VERSION "5.3.3"
+/* #undef GRAPHICS_DISABLED */
+#define FAST_FLOAT ON
+/* #undef DISABLED_LEGACY_ENGINE */
+#define HAVE_TIFFIO_H ON
+/* #undef HAVE_NEON */
+/* #undef HAVE_LIBARCHIVE */
+/* #undef HAVE_LIBCURL */
+/* #undef USE_OPENCL */
---
+++ 3rdparty/TesserAct/include/tesseract/config_auto.h
@@ -0,0 +1,60 @@
+/* Define to 1 if you have the <dlfcn.h> header file. */
+/* #undef HAVE_DLFCN_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+/* #undef HAVE_UNISTD_H */
+
+/* Define to 1 if you have the `fmemopen' function. */
+/* #undef HAVE_FMEMOPEN */
+
+/* Define to 1 if you have the `fstatat' function. */
+/* #undef HAVE_FSTATAT */
+
+
+/* Define to 1 if you have giflib. */
+/* #undef HAVE_LIBGIF */
+
+/* Define to 1 if you have libopenjp2. */
+#define HAVE_LIBJP2K 1
+
+/* Define to 1 if you have jpeg. */
+#define HAVE_LIBJPEG 1
+
+/* Define to 1 if you have libpng. */
+#define HAVE_LIBPNG 1
+
+/* Define to 1 if you have libtiff. */
+#define HAVE_LIBTIFF 1
+
+/* Define to 1 if you have libwebp. */
+#define HAVE_LIBWEBP 1
+
+/* Define to 1 if you have libwebpmux. */
+/* #undef HAVE_LIBWEBP_ANIM */
+
+/* Define to 1 if you have zlib. */
+#define HAVE_LIBZ 1
